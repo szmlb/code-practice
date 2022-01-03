a40664278d0d7451674500ae35fc6374d8df8209
szmlb <szmlb.robotics@gmail.com>
2022-01-02 21:45:33 -0800

    Corrected separateBacktrack.jl to work as expected

--- julia/programming_treasure_chest/src/separateBacktrack.jl
+++ julia/programming_treasure_chest/src/separateBacktrack.jl
@@ -1,74 +1,107 @@
 using Printf
+using Logging
+
+Logging.disable_logging(Logging.Info)
+
+mutable struct separate_data
+    n::Int
+    separator::Int
+    value::Array{Int}
+    sep_pos::Array{Int}
+    best_sep_pos::Array{Int}
+    best_sep_max_value::Int
+    separate_data() = new()
+end
 
 # 分割を実装する再帰関数
-function separate(pos, num)
+function separate(sd, pos, num)
 
     # 新しい分割場所を設定
-    sep_pos[num] = pos
+    sd.sep_pos[num] = pos
     num = num + 1
 
     # 分割がすべて決まったら
-    if num == SEPARATOR
+    if num == sd.separator+1
         max = 0
 
         # 設定された分割で，最大のグループ和を算出する
-        for i in 1:SEPARATOR
-            start = (i == 0) ? 0: sep_pos[i-1]
-            end = (i == SEPARATOR)? N: sep_pos[i]
+        for i in 1:sd.separator+1
+            if i == 1
+                _start = 1
+                _end = sd.sep_pos[i]
+            elseif i == sd.separator+1
+                _start = sd.sep_pos[i-1]+1
+                _end = sd.n
+            else
+                _start = sd.sep_pos[i-1]+1
+                _end = sd.sep_pos[i]
+            end
             k = 0
-            for j in start:end
-                k = k + value[j]
+            for j in _start:_end
+                k = k + sd.value[j]
             end
             if k > max
                 max = k
             end
         end
 
+        @info "sep_pos" sd.sep_pos
+
         # 最大のグループ和が保存されている和より小さければ
-        if max < best_sep_max_value
+        if max < sd.best_sep_max_value
             # 現在の分割を保存する
-            best_sep_max_value = max
-            for i in 1:SEPARATOR
-                best_sep_pos[i] = sep_pos[i]
+            sd.best_sep_max_value = max
+            for i in 1:sd.separator
+                sd.best_sep_pos[i] = sd.sep_pos[i]
             end
         end
         return
     end
 
     # 次の分割場所を設定する
-    for i in pos+1:N-SEPARATOR+num+1
-        separate(i, num)
+    for i_pos in pos+1:sd.n-sd.separator+num
+        separate(sd, i_pos, num)
     end
 end
 
 function main()
 
     # 与えられた値と，その分割方法
-    N = 10
-    SEPARATOR = 3
-    value = [15, 3, 7, 6, 10, 4, 13, 2, 3, 6]
-    sep_pos = zeros(SEPARATOR)
-
+    sd  = separate_data()
+    sd.n = 10
+    sd.separator = 3
+    sd.value = [15, 3, 7, 6, 10, 4, 13, 2, 3, 6]
+    sd.sep_pos = zeros(sd.separator)
     # 最適な分割と，そのなかのグループの最大和
-    best_sep_pos = zeros(SEPARATOR)
-    best_sep_max_value = 9999
+    sd.best_sep_pos = zeros(sd.separator)
+    sd.best_sep_max_value = 9999
 
     # 最初の分割場所を設定して再帰を呼び出す
-    for i in 1:N-SEPARATOR+1
-        separate(i, 0)
+    for i in 1:sd.n-sd.separator+1
+        separate(sd, i, 1)
     end
 
     # 保存された分割場所を表示する
-    for i in 1:SEPARATOR
-        start = (i == 0)? 0: best_sep_pos[i - 1]
-        end = (i == SEPARATOR)? N: best_sep_pos[i]
-        for j in start:end
-            @printf "%d " value[j]
+    for i in 1:sd.separator+1
+        if i == 1
+            _start = 1
+            _end = sd.best_sep_pos[i]
+        elseif i == sd.separator+1
+            _start = sd.best_sep_pos[i-1]+1
+            _end = sd.n
+        else
+            _start = sd.best_sep_pos[i-1]+1
+            _end = sd.best_sep_pos[i]
         end
-        if end != N
-            @printf "|"
+
+        for j in _start:_end
+            @printf "%d " sd.value[j]
+        end
+        if _end != sd.n
+            @printf "| "
         end
     end
+    @printf " max value: %d\n" sd.best_sep_max_value
 
     return true
 end
