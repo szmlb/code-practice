7142cfc23fa54ee14179bfbf8e6068b61bbb9b0d
szmlb <szmlb.robotics@gmail.com>
2022-11-13 21:20:16 +0900

    Modified code6-1.rs

--- rust/algorithm_and_data_structure/src/bin/code6-1.rs
+++ rust/algorithm_and_data_structure/src/bin/code6-1.rs
@@ -1,34 +1,29 @@
-fn binary_search(key: usize, arr: &mut Vec<usize>) -> usize{
+fn binary_search(key: i32, data: &Vec<i32>) -> i32{
 
-    let mut left: usize = 0;
-    let mut right: usize = arr.len() - 1;
-    while right >= left {
+    let mut left = 0;
+    let mut right = data.len() - 1;
+   
+    while right >= left{
         let mid = left + (right - left) / 2;
-        if arr[mid] == key {return mid;}
-        else if arr[mid] > key {right = mid - 1;}
-        else if arr[mid] < key {left = mid + 1;}
+        if data[mid] == key{
+            return mid as i32
+        } else if data[mid] > key{
+            right = mid - 1;
+        } else if data[mid] < key{
+            left = mid + 1;
+        }
     }
-    return usize::MAX;
+    -1
 }
 
 fn main() {
-    let n: usize = 8;
-    let mut arr: Vec<usize> = Vec::with_capacity(n);
-    arr.resize(n, 0);
-    arr[0] = 3;
-    arr[1] = 5;
-    arr[2] = 8;
-    arr[3] = 10;
-    arr[4] = 14;
-    arr[5] = 17;
-    arr[6] = 21;
-    arr[7] = 39;
+    let data: Vec<i32> = vec![3, 5, 8, 10, 14, 17, 21, 39];
 
-    println!("{}", binary_search(10, &mut arr));
-    println!("{}", binary_search(3, &mut arr));
-    println!("{}", binary_search(39, &mut arr));
+    println!("{}", binary_search(10, &data));
+    println!("{}", binary_search(3, &data));
+    println!("{}", binary_search(39, &data));
 
-    println!("{}", binary_search(100, &mut arr));
-    println!("{}", binary_search(125, &mut arr));
-    println!("{}", binary_search(300, &mut arr));
+    println!("{}", binary_search(100, &data));
+    println!("{}", binary_search(125, &data));
+    println!("{}", binary_search(300, &data));
 }
