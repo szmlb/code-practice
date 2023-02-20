99664837c5ff37b567580031f2cfb1142f5327a7
szmlb <szmlb.robotics@gmail.com>
2023-02-20 23:53:00 +0900

    Add stack_heap_example

---
+++ cpp/stack_heap_example/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+int main()
+{
+
+    Vector2d a;
+    Vector3d b;
+    VectorXd c = VectorXd::Zero(8);
+    Matrix<double, 8, 1> d;
+    Matrix<double, 8, Dynamic> e;
+
+}
