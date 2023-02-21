acc39c947fb33e99f941ac9854880904eb4c5d82
szmlb <szmlb.robotics@gmail.com>
2023-02-21 23:57:48 +0900

    Added an example to main.cpp

--- cpp/stack_heap_example/main.cpp
+++ cpp/stack_heap_example/main.cpp
@@ -3,13 +3,20 @@
 
 using namespace Eigen;
 
+using Vector8d = Matrix<double, 8, 1>;
+using Matrix8d = Matrix<double, 8, 8>;
+
 int main()
 {
+
+    constexpr int n = 8;
 
     Vector2d a;
     Vector3d b;
     VectorXd c = VectorXd::Zero(8);
-    Matrix<double, 8, 1> d;
-    Matrix<double, 8, Dynamic> e;
+    Matrix<double, n, 1> d;
+    Matrix<double, n, Dynamic> e;
+    Matrix<double, Dynamic, Dynamic> f;
+    Vector8d g;
 
 }
